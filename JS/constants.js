c380d943810202884aaa15708ea577c5cb84bc63
Mahmoud Elsayed <mr.houda2001@gmail.com>
2025-08-10 17:28:46 +0300

    ..

--- JS/constants.js
+++ JS/constants.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/";
+const API_URL = "https://work-backend-production.up.railway.app/";
 
 //  "https://work-backend-production.up.railway.app/"
 // http://localhost:3000/
