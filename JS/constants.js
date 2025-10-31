f77b08c3018f5fd0417a1412b107ee795a209fb7
mahmoudElsayedNPC <mr.houda2002@gmail.com>
2025-10-31 13:50:54 +0200

    add some styles

--- JS/constants.js
+++ JS/constants.js
@@ -1,7 +1,7 @@
-const API_URL = "http://localhost:3000/";
+const API_URL = "http://192.168.0.38:3000/";
 
-//  "https://work-backend-production.up.railway.app/"
 // http://localhost:3000/
+// http://192.168.0.38:3000/
 
 const permissions = {
   admin: [
